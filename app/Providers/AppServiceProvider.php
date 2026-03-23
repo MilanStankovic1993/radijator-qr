c8387fe7cadcd8cc840a7b4999d4c3349aaf88d4
Milan <tvoj@email.com>
2026-03-23 12:38:15 +0100

    * vracam fajl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        if (app()->environment('production')) {
+        if (app()->environment('production') && request()->isSecure()) {
             URL::forceScheme('https');
         }
 
